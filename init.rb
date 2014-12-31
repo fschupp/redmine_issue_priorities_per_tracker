2522b9e3d22502ce73837910503b10283773bef9
fschupp <fschupp@outlook.com>
2014-12-31 15:17:00 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -7,11 +7,11 @@ require File.dirname(__FILE__) + '/lib/hooks.rb'
 
 Redmine::Plugin.register :redmine_issue_priorities_per_tracker do
   name 'Redmine Issue Priorities Per Tracker plugin'
-  author 'Andrey Kolashtov <andrey.kolashtov@flant.ru>'
+  author 'Felix Schupp <fschupp@outlook.com> (forked from Andrey Kolashtov)'
   description 'This is a plugin for Redmine which allows to define issue priorities per tracker.'
-  version '0.0.1'
-  url 'https://github.com/flant/redmine_issue_priorities_per_tracker'
-  author_url 'https://github.com/kolashtov'
+  version '0.0.2'
+  url 'https://github.com/fschupp/redmine_issue_priorities_per_tracker'
+  author_url 'https://github.com/fschupp'
   
   menu :admin_menu, :issue_priorities_per_tracker, { :controller => 'issue_priorities_per_tracker' }, :caption => :label_issue_priorities_per_tracker
 end
