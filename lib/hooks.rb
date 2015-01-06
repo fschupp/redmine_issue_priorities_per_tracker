83d9f179b254d459e39eb65ff51136084e2cc40b
Felix Schupp <fschupp@outlook.com>
2015-01-06 12:44:07 +0100

    Update hooks.rb

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -2,4 +2,9 @@ class RedmineIssuePrioritiesPerTrackerHookListener < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context)
       stylesheet_link_tag 'main', :plugin => :redmine_issue_priorities_per_tracker
   end
+
+end
+
+class Hooks < Redmine::Hook::ViewListener
+  render_on(:view_layouts_base_html_head, :partial => "html_head")
 end
