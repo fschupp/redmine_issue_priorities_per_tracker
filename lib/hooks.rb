fdf10e5411a34c327e8925ccc70776a982920188
Felix Schupp <fschupp@outlook.com>
2015-01-06 13:00:12 +0100

    Update hooks.rb

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,10 +1,12 @@
 class RedmineIssuePrioritiesPerTrackerHookListener < Redmine::Hook::ViewListener
 
   def view_layouts_base_html_head(context)
-      stylesheet_link_tag 'main', :plugin => :redmine_issue_priorities_per_tracker +
+      stylesheet_link_tag 'main', :plugin => :redmine_issue_priorities_per_tracker
       
-      javascript_include_tag 'prototype.js', :plugin => :redmine_prototype_js +
+      javascript_include_tag 'prototype.js', :plugin => :redmine_prototype_js
       javascript_include_tag 'prototype_ujs.js', :plugin => :redmine_prototype_js 
+      
+     # javascript_include_tag("select2.min.js", :plugin => "redmine_base_select2")
   end
 
 end
